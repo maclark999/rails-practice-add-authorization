7488f3ab70f769d1a80afa693eb2f55333420fab
mackenzie clarkson <maclark99@gmail.com>
2015-02-17 13:31:00 -0700

    only allow access if signed in, limit access to club pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_student
-    Student.find_by_id(session[:student_id])
+    @current_student ||= Student.find_by_id(session[:student_id])
+  end
+
+  def authenticate
+    if not current_student
+      flash[:notice]= "Not Signed in"
+      redirect_to login_path
+    end
   end
 
   helper_method :current_student
--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,11 +1,19 @@
 class ClubsController < ApplicationController
 
+  before_action :authenticate
+
   def index
     @clubs = Club.all
   end
 
   def show
     @club = Club.find(params[:id])
+    @student = current_student.clubs
+
+    unless @student.include? @club
+      render 'public/403.html'
+
+    end
   end
 
 end
